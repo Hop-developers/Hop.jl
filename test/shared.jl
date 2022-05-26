056f2c27603afd8dae247c46dffbf2a110bb52b8
Chong Wang <ch-wang@outlook.com>
2022-05-25 21:50:10 -0700

    fix ci

--- test/shared.jl
+++ test/shared.jl
@@ -1,7 +1,7 @@
 using Hop, Test
 
 let
-    tm = Hop.Interface.createmodelopenmx(joinpath(dirname(@__FILE__), "HopTestData", "GaAs.openmx39"))
+    tm = Hop.Interface.createmodelopenmx(joinpath(dirname(@__FILE__), "data", "GaAs.openmx39"))
     sm = SharedTBModel(tm)
     tm_result = Hop.Optics.get_shg(tm, 1, 2, 3, [1.5], -4.2, [5, 5, 5]; ϵ=0.1, scissor=0.69)[1]
     sm_result = Hop.Optics.get_shg(sm, 1, 2, 3, [1.5], -4.2, [5, 5, 5]; ϵ=0.1, scissor=0.69)[1]
