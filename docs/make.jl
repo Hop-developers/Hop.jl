87a8eb59aa1555db1e4a180547e8cb1e4961f54f
Chong Wang <ch-wang@outlook.com>
2022-05-25 22:29:47 -0700

    Github specific updates (#1)

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,7 @@
 using Documenter, Hop
 
-makedocs(
-    sitename="Hop",
-    repo="https://gitlab.com/mistguy/Hop.jl/blob/{commit}{path}#{line}",
-    pages = [
-        "Homepage" => "index.md",
-        "Hop" => "hop.md",
-        "Hop.Optics" => "optics.md",
-        "Hop.Hall" => "hall.md",
-        "Hop.BandStructure" => "bs.md",
-        "Hop.Floquet" => "floquet.md",
-        "Hop.Response" => "response.md",
-        "Hop.Group" => "group.md",
-        "Hop.Topology" => "topology.md",
-        "Hop.Wannier" => "wannier.md",
-        "Hop.Magnetism" => "magnetism.md",
-        "Hop.Utilities" => "utilities.md",
-    ]
+makedocs(sitename="Hop.jl")
+
+deploydocs(
+    repo = "github.com/Hop-developers/Hop.jl.git"
 )
